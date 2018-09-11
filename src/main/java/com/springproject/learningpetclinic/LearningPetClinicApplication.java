fb0a1fdc4ec64bb706db88f0d51b718dd3efad23
Maria Donenko <mariadonenko@gmail.com>
2018-09-11 23:13:54 +0300

    Init Project

---
+++ src/main/java/com/springproject/learningpetclinic/LearningPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.springproject.learningpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningPetClinicApplication.class, args);
+	}
+}
