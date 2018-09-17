3c6117eeef32fd53f71aeb2b444acf762898d9c1
Maria Donenko <mariadonenko@gmail.com>
2018-09-17 23:39:34 +0300

    refactoring to crud interface

--- pet-clinic-data/src/main/java/com/springproject/learningpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/springproject/learningpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.springproject.learningpetclinic.services;
 
 import com.springproject.learningpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }
