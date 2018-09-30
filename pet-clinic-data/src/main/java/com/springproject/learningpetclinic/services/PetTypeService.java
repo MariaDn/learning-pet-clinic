598f82cde330998cfd363a6f402902f98b6b27bc
Maria Donenko <mariadonenko@gmail.com>
2018-09-30 17:38:27 +0300

    added PetTypeService

---
+++ pet-clinic-data/src/main/java/com/springproject/learningpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springproject.learningpetclinic.services;
+
+import com.springproject.learningpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
