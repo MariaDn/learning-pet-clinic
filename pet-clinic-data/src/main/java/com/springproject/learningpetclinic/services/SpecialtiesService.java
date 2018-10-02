a298873357f205d50cccb235e36d9c7fdc2b0095
Maria Donenko <mariadonenko@gmail.com>
2018-10-02 22:29:36 +0300

    added Specialties Map Service

---
+++ pet-clinic-data/src/main/java/com/springproject/learningpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.springproject.learningpetclinic.services;
+
+import com.springproject.learningpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
