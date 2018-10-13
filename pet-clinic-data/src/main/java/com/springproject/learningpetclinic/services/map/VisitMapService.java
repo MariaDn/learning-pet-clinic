aa85a9e0725808912f348b4acb6eb0a60ad2086e
Maria Donenko <mariadonenko@gmail.com>
2018-10-13 20:08:27 +0300

    implemented jpa for visit service, added info ti data loader

--- pet-clinic-data/src/main/java/com/springproject/learningpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/com/springproject/learningpetclinic/services/map/VisitMapService.java
@@ -2,9 +2,11 @@ package com.springproject.learningpetclinic.services.map;
 
 import com.springproject.learningpetclinic.model.Visit;
 import com.springproject.learningpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
     @Override
     public Set<Visit> findAll() {
--- pet-clinic-web/src/main/java/com/springproject/learningpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springproject/learningpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.springproject.learningpetclinic.bootstrap;
 
 import com.springproject.learningpetclinic.model.*;
-import com.springproject.learningpetclinic.services.OwnerService;
-import com.springproject.learningpetclinic.services.PetTypeService;
-import com.springproject.learningpetclinic.services.SpecialtyService;
-import com.springproject.learningpetclinic.services.VetService;
+import com.springproject.learningpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
 
@@ -88,6 +87,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(francoisCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(francoisCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Kitty flue");
+
+        visitService.save(catVisit);
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Adam");
