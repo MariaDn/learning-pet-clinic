94fbffd91f1c7b4cd3b7d8c03b47c45e1244d305
Maria Donenko <mariadonenko@gmail.com>
2018-10-25 22:50:17 +0300

    fix OwnerServiceMapTest

---
+++ pet-clinic-data/src/test/java/com/springproject/learningpetclinic/services/map/OwnerServiceMapTest.java
@@ -0,0 +1,35 @@
+package com.springproject.learningpetclinic.services.map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OwnerServiceMapTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+}
